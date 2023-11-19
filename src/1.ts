702a1d68f1750d5cf51ba5222e7db7288c8f981a
Serghii.Kolodiazhnyi <139024405+AsphaltHedgehog@users.noreply.github.com>
2023-11-20 00:40:07 +0200

    fixed

--- src/1.ts
+++ src/1.ts
@@ -10,9 +10,6 @@ class Student {
     public age: number,
     public grade: string
   ) {
-    this.name = name;
-    this.age = age;
-    this.grade = grade;
   }
 }
 
--- src/4.ts
+++ src/4.ts
@@ -10,45 +10,38 @@ class Key {
 }
 
 class Person {
-  constructor(public key: number) {
-    this.key = key;
+  constructor(public key: Key) {
   }
 
-  getKey(): number {
+  getKey(): Key {
     return this.key
   };
 }
 
 abstract class House {
-  tenants: Person[] = [];
-  constructor(protected door: boolean, protected key: Key) {
-    this.door = door;
+  private tenants: Person[] = [];
+  private door: boolean = false;
+  constructor( protected key: Key) {
     this.key = key;
-    this.tenants = [];
   } 
   
-  comeIn(person: Person) {
+  comeIn(person: Person): void {
     if (!this.door) {
       return console.log("door is closed");
     }
     this.tenants.push(person)
   }
 
-  abstract openDoor(key: number):void
+  abstract openDoor(key: Key): void;
 }
 
-class MyHouse extends House {
-  constructor(key: Key) {
-    super(false, key)
-  }
+class MyHouse extends House { 
 
-  openDoor(key: number): void {
-    if (key !== this.key.getSignature()) {
+  openDoor(key: Key): void {
+    if (key.getSignature() !== this.key.getSignature()) {
       return console.log('Invalid key');
     }
-    this.door = true;
     console.log('Door is opened');
-    
   }
 }
 
@@ -56,7 +49,7 @@ class MyHouse extends House {
 const key = new Key();
 
 const house = new MyHouse(key);
-const person = new Person(key.getSignature());
+const person = new Person(key);
 
 house.openDoor(person.getKey());
 
